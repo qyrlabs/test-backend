760d137990ef0d5f2d36c1855a08b450a92f2024
Vladislav Byrgazov <vlados.byrgazov@gmail.com>
2026-01-25 11:30:32 +0000

    feat: start inventory grpc server
    Signed-off-by: Vladislav Byrgazov <vlados.byrgazov@gmail.com>

--- inventory/cmd/main.go
+++ inventory/cmd/main.go
@@ -1,9 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	inventoryv1 "github.com/qyrlabs/test-backend/shared/pkg/proto/inventory/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
+const grpcPort = 50061
+
+type inventoryService struct {
+	inventoryv1.UnimplementedInventoryServiceServer
+
+	mu    sync.RWMutex
+	parts map[string]*inventoryv1.Part
+}
+
 func main() {
-	log.Println("inventory up")
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", grpcPort))
+	if err != nil {
+		log.Printf("failed to listen: %v\n", err)
+		return
+	}
+
+	defer func() {
+		if cerr := lis.Close(); cerr != nil {
+			log.Printf("failed to close listener: %v\n", cerr)
+		}
+	}()
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	service := &inventoryService{
+		parts: make(map[string]*inventoryv1.Part),
+	}
+
+	inventoryv1.RegisterInventoryServiceServer(grpcServer, service)
+
+	go func() {
+		log.Printf("gRPC server listening on %s\n", lis.Addr().String())
+		err := grpcServer.Serve(lis)
+		if err != nil {
+			log.Printf("Failed to serve: %v\n", err)
+			return
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down gRPC server...")
+	grpcServer.GracefulStop()
+	log.Println("gRPC server stopped")
 }
--- inventory/go.mod
+++ inventory/go.mod
@@ -1,3 +1,19 @@
 module github.com/qyrlabs/test-backend/inventory
 
 go 1.25.6
+
+replace github.com/qyrlabs/test-backend/shared => ../shared
+
+require (
+	github.com/qyrlabs/test-backend/shared v0.0.0-00010101000000-000000000000
+	google.golang.org/grpc v1.78.0
+)
+
+require (
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/genproto v0.0.0-20260122232226-8e98ce8d340d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260120174246-409b4a993575 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
